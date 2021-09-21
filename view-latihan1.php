b8d3660d4c7b7a0a842972ee1d17d5c71495032d
Syadad13 <90832478+Syadad13@users.noreply.github.com>
2021-09-21 20:52:40 +0700

    tugas wp2
    mengumpulkan tugas wp2 latihan1

---
+++ view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+ <title>Latihan 1</title>
+</head>
+<body>
+ <h1>Halo Sahabat.. Mari belajar web programming..!!!</h1><br>
+ Nilai 1 = <?= $nilai1; ?>
+ Nilai 2 = <?= $nilai2; ?>
+ ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
