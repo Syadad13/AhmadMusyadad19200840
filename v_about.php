4c30b1ca8d99f8e6462e05bf2f87aacb6dd8b0ea
Syadad13 <90832478+Syadad13@users.noreply.github.com>
2021-10-12 23:47:56 +0700

    membuat web dengan login dan register

---
+++ v_about.php
@@ -0,0 +1,22 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Lengkap : Ahmad Musyadad</li> 
+        <li>NIM          : 19200840</li> 
+        <li>Kelas        : 19.3A.02</li> 
+    </ul> 
+    <br> 
+    <h4>Mata Kuliah</h4> 
+    <ul type="none"> 
+        <li> WEB PROGRAMMING II </li> 
+    </ul> 
+    <h4>Jurusan</h4> 
+    <ul type="none"> 
+        <li>Sistem Informasi</li> </ul> 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Bulutangkis</li> 
+        <li>Basket</li> 
+    </ul> 
+</section>
